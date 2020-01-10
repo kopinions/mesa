a3ca68f6a4aa80fa18cc62b5a6bc97148060807a
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-01-10 22:30:45 +0100

    nir: remove duplicate optimization
    This one was already added in 9fdaeb7776c5, no point in doing it twice.

--- src/compiler/nir/nir_opt_algebraic.py
+++ src/compiler/nir/nir_opt_algebraic.py
@@ -480,7 +480,6 @@ optimizations.extend([
    (('fmin', ('fmin', a, b), b), ('fmin', a, b)),
    (('umin', ('umin', a, b), b), ('umin', a, b)),
    (('imin', ('imin', a, b), b), ('imin', a, b)),
-   (('fmax', a, ('fneg', a)), ('fabs', a)),
    (('imax', a, ('ineg', a)), ('iabs', a)),
    (('fmin', a, ('fneg', a)), ('fneg', ('fabs', a))),
    (('imin', a, ('ineg', a)), ('ineg', ('iabs', a))),
